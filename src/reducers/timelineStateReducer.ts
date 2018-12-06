7d562516193b8b0c2332d552e50f9e380d30546c
Behnam Rasooli <ben.rasooli@gmail.com>
2018-12-06 21:28:59 +1100

    Add changing Map Marker upon receiving activeProfile from api

--- src/reducers/timelineStateReducer.ts
+++ src/reducers/timelineStateReducer.ts
@@ -1,12 +1,16 @@
 import * as constants from "src/constants";
 import { AppState } from ".";
-import { mapActions, TimelineChangedAction } from "src/actions";
+import { GetActiveProfileAction, mapActions, TimelineChangedAction } from "src/actions";
+
+
 
 export default function(
   prevState: AppState,
-  action: TimelineChangedAction | mapActions
+  action: TimelineChangedAction | mapActions | GetActiveProfileAction
 ): AppState["timelineState"] {
   switch (action.type) {
+    case `${constants.GET_ACTIVE_PROFILE}_FULFILLED`:
+      return { ...prevState.timelineState, isChanged: true };
     case constants.TIMELINE_CHANGED:
       return { ...prevState.timelineState, isChanged: true };
     case constants.MAP_STARTED_TO_FLY:
