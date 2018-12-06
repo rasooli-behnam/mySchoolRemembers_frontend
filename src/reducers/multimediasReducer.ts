290fdb7da17c97cde14b37f5d3765e39028c5a66
Behnam Rasooli <ben.rasooli@gmail.com>
2018-12-06 21:07:47 +1100

    Add loading Multimedia upon receiving activeProfile from api

--- src/reducers/multimediasReducer.ts
+++ src/reducers/multimediasReducer.ts
@@ -1,18 +1,31 @@
 import * as constants from "src/constants";
+import Multimedia from "src/dataTypes/Multimedia";
 import { AppState } from ".";
-import { OpenMultimediaComponentAction } from "src/actions";
+import { GetActiveProfileAction, OpenMultimediaComponentAction } from "src/actions";
+
+
+
+const sortBy = require("lodash.sortby");
 
 export default function(
   prevState: AppState,
-  action: OpenMultimediaComponentAction
+  action: OpenMultimediaComponentAction | GetActiveProfileAction
 ) {
   if (action.type === constants.OPEN_MULTIMEDIA_COMPONENT) {
-    const payload = action.payload;
-    if (payload) {
-      return prevState.activeProfile.multimedias.filter(
-        m => m.date === payload.date
-      );
-    } else return prevState.activeProfile.multimedias;
+    return prevState.activeProfile.multimedias.filter(
+      m => m.date === action.payload.date
+    );
+  }
+
+  if (action.type === `${constants.GET_ACTIVE_PROFILE}_FULFILLED`) {
+    const sortedMultimedias: Multimedia[] = sortBy(
+      action.payload.data.multimedias,
+      "date"
+    );
+
+    return sortedMultimedias.filter(
+      m => m.date === sortedMultimedias[0].date //initial date
+    );
   }
 
   return prevState && prevState.multimedias ? prevState.multimedias : [];
