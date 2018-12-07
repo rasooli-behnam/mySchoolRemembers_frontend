b0c6625f58e1792dd515e46266c20b0935c7a991
Behnam Rasooli <ben.rasooli@gmail.com>
2018-12-07 17:06:02 +1100

    Fix map transitioning bug - it chokes when same coords are suplied, therefore never calls onTransitionEnd

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -47,6 +47,7 @@ export default class Map extends React.Component<Props, State> {
     };
 
     this.setState({ viewport, marker });
+    this.props.mapStoppedFlying(); //this is for fixng the bug - if same coords are suplied, map never starts transitioning
   };
 
   // smaller zoom level results in bigger offset value
@@ -79,6 +80,7 @@ export default class Map extends React.Component<Props, State> {
         onViewportChange={viewport => {
           this.setState({ viewport });
         }}
+        onTransitionStart={() => this.props.mapStartedToFly()}
         onTransitionEnd={() => this.props.mapStoppedFlying()}
       >
         <Marker
